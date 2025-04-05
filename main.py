70a106d2e1d1dfb6a05625886d11255f891bdf06
Sotades <anthonyjbateman@gmail.com>
2025-04-05 21:30:11 +0300

    Add Langchain app already started logic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QTimer
 
 import PySide6.QtAsyncio as QtAsyncio
 
@@ -18,12 +18,23 @@ class MainWindow(QMainWindow):
         # Set the central widget of the Window.
         self.setCentralWidget(button)
 
+        self.langgraph_app_status = 'Not started'
+
     def start_langchain_app(self):
-        print("Starting Langchain Code Thread")
-        # Create thread and start it.
-        loop = asyncio.get_running_loop()
-        self.langgraph_thread = LangGraphThread(loop=loop)
-        self.langgraph_thread.start()
+        if self.langgraph_app_status == 'Started':
+            print('Already started')
+        else:
+            self.langgraph_app_status = 'Started'
+            print("Starting Langchain Code Thread")
+
+            # Create thread and start it.
+            loop = asyncio.get_running_loop()
+            self.langgraph_thread = LangGraphThread(loop=loop)
+            self.langgraph_thread.start()
+
+    def check_for_langgraph_data(self):
+        print("Checking for Langgraph Data")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
