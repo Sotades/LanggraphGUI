ae835060d8297a63b4b068fb13565285fc7e8433
Sotades <anthonyjbateman@gmail.com>
2025-04-09 21:39:45 +0300

    Add working Langgraph example

---
+++ langgraph_asyncio.py
@@ -0,0 +1,119 @@
+import asyncio
+from dotenv import load_dotenv
+result = load_dotenv()
+
+from typing import Annotated
+
+from typing_extensions import TypedDict
+
+from langgraph.graph.message import add_messages
+
+# Add messages essentially does this with more
+# robust handling
+# def add_messages(left: list, right: list):
+#     return left + right
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+from langchain_core.tools import tool
+
+
+@tool
+def search(query: str):
+    """Call to surf the web."""
+    # This is a placeholder, but don't tell the LLM that...
+    return ["The answer to your question lies within."]
+
+
+tools = [search]
+
+from langgraph.prebuilt import ToolNode
+
+tool_node = ToolNode(tools)
+
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI(model="gpt-4o")
+
+model = model.bind_tools(tools)
+
+from typing import Literal
+
+
+# Define the function that determines whether to continue or not
+def should_continue(state: State) -> Literal["end", "continue"]:
+    messages = state["messages"]
+    last_message = messages[-1]
+    # If there is no tool call, then we finish
+    if not last_message.tool_calls:
+        return "end"
+    # Otherwise if there is, we continue
+    else:
+        return "continue"
+
+
+# Define the function that calls the model
+async def call_model(state: State):
+    messages = state["messages"]
+    response = await model.ainvoke(messages)
+    # We return a list, because this will get added to the existing list
+    return {"messages": [response]}
+
+from langgraph.graph import END, StateGraph, START
+
+# Define a new graph
+workflow = StateGraph(State)
+
+# Define the two nodes we will cycle between
+workflow.add_node("agent", call_model)
+workflow.add_node("action", tool_node)
+
+# Set the entrypoint as `agent`
+# This means that this node is the first one called
+workflow.add_edge(START, "agent")
+
+# We now add a conditional edge
+workflow.add_conditional_edges(
+    # First, we define the start node. We use `agent`.
+    # This means these are the edges taken after the `agent` node is called.
+    "agent",
+    # Next, we pass in the function that will determine which node is called next.
+    should_continue,
+    # Finally we pass in a mapping.
+    # The keys are strings, and the values are other nodes.
+    # END is a special node marking that the graph should finish.
+    # What will happen is we will call `should_continue`, and then the output of that
+    # will be matched against the keys in this mapping.
+    # Based on which one it matches, that node will then be called.
+    {
+        # If `tools`, then we call the tool node.
+        "continue": "action",
+        # Otherwise we finish.
+        "end": END,
+    },
+)
+
+# We now add a normal edge from `tools` to `agent`.
+# This means that after `tools` is called, `agent` node is called next.
+workflow.add_edge("action", "agent")
+
+# Finally, we compile it!
+# This compiles it into a LangChain Runnable,
+# meaning you can use it as you would any other runnable
+app = workflow.compile()
+
+async def main():
+    from langchain_core.messages import HumanMessage
+
+    inputs = {"messages": [HumanMessage(content="what is the weather in sf")]}
+    messages = await app.ainvoke(inputs)
+
+    for m in messages['messages']:
+        m.pretty_print()
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
